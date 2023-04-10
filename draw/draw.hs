7e2bdbbb6894f7b0003e6ffe401298c264ea813a
Duncan <antigravityd@gmail.com>
2023-04-10 15:07:32 -0500

    Bruh Moment

--- draw/draw.hs
+++ draw/draw.hs
@@ -1,1 +1,15 @@
-bruh1 :: (Integer, Integer, Integer, Integer) ->
+data Side = Left | Right
+
+ntoxy :: Integer -> Side -> (Real, Real)
+ntoxy n s = if Side == Left then (0.0, )
+
+noshift :: Integer -> Integer -> Integer -> [(Integer, Integer)]
+noshift p a b = map (\x -> | x <= 2 * a = x + b
+                           | x <= 2 * a + b = x - 2 * a
+                           | x) [1..p]
+
+
+
+pabr_adjacency :: Integer -> Integer -> Integer -> Integer -> [(Integer, Integer)]
+pabr_adjacency p a b r = let rolled = map (\x -> 1 + mod (x + r - 1) p) $ noshift p a b
+                             in zip [1..p] rolled
